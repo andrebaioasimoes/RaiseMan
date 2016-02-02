f9008d294eb55cb2879f1fabd2854702baa87d54
Andre Simoes <andrebaioasimoes@gmail.com>
2016-02-02 01:09:49 +0000

    adding files and folders

---
+++ RaiseMan/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  RaiseMan
+//
+//  Created by André Simões on 02/02/16.
+//  Copyright © 2016 André Simões. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ RaiseMan/Employee.swift
@@ -0,0 +1,14 @@
+//
+//  Employee.swift
+//  RaiseMan
+//
+//  Created by André Simões on 02/02/16.
+//  Copyright © 2016 André Simões. All rights reserved.
+//
+
+import Foundation
+
+class Employee: NSObject {
+    var name: String? = "New Employee"
+    var raise: Float = 0.05
+}
